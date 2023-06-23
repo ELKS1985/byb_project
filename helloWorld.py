1d21f178d6d3d7e8892d24ea66ac608b78d483ac
Rachael Joan Elkington <127788109+ELKS1985@users.noreply.github.com>
2023-06-23 14:36:59 +0100

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,19 +1,23 @@
 """
-Some various ways of saying Hello World in Python
+Below are three different ways to print "Hello World" using Python.
 """
-
+# -------------------------------------------------------------------------
+# 1. Print out "Hello World":
+print("Hello World")
+# -------------------------------------------------------------------------
+# 2. Assign the string 'Hello World' to a variable, then print it out:
+str = "Hello World"
+print(str)
+# -------------------------------------------------------------------------
+# 3. Define a function that prints out "Hello World":
 def hello():
     """
     Print out Hello World in a function.
     """
     print("Hello World in a function.")
-
-# Call a function that prints out Hello World
+# Call function to print "Hello World"
 hello()
+# -------------------------------------------------------------------------
 
-# Print out Hello World 
-print("Just saying Hello World")
 
-# Assign the string Hello World to a variable and print it out
-str = "Hello World in a variable"
-print(str)
+
